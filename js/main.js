5c875b253184a9a128ba4d68907b36b144293f11
simplebutuseful <simplebutuseful@interia.pl>
2024-03-04 22:15:57 +0100

    more flexible styling options
    Signed-off-by: simplebutuseful <simplebutuseful@interia.pl>

--- js/main.js
+++ js/main.js
@@ -18,8 +18,22 @@ function create(){
 	document.getElementById("nav").style.backgroundColor = document.getElementById("myNavBgColor").value;
 	document.getElementById("title").style.color = document.getElementById("myColor").value;
 	document.getElementById("title").style.backgroundColor = document.getElementById("myBgColor").value;
+	document.getElementById("lead").style.color = document.getElementById("myColor").value;
+	document.getElementById("lead").style.backgroundColor = document.getElementById("myBgColor").value;
 	document.getElementById("output").style.backgroundColor = document.getElementById("myBgColor").value;
-	//set image
+	//font families
+	document.getElementById("nav").style.fontFamily = document.getElementById("myNavFontFamily").value;
+	document.getElementById("title").style.fontFamily = document.getElementById("myTitleFontFamily").value;
+	document.getElementById("lead").style.fontFamily = document.getElementById("myLeadFontFamily").value;
+	//font sizes
+	document.getElementById("nav").style.fontSize = document.getElementById("myNavFontSize").value;
+	document.getElementById("title").style.fontSize = document.getElementById("myTitleFontSize").value;
+	document.getElementById("lead").style.fontSize = document.getElementById("myLeadFontSize").value;
+	//font weight
+	document.getElementById("nav").style.fontWeight = document.getElementById("myNavFontWeight").value;
+	document.getElementById("title").style.fontWeight = document.getElementById("myTitleFontWeight").value;
+	document.getElementById("lead").style.fontWeight = document.getElementById("myLeadFontWeight").value;
+	//add image
 	document.getElementById("image").src = URL.createObjectURL(document.getElementById("myImage").files[0]);
 }
 
